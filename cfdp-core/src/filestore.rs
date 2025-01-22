2d8ca992c79ba8a2b7c49a9fe07f4215907e31e0
Jose Manuel Diez <j.diezlopez@tu-berlin.de>
2025-01-22 12:39:39 +0100

    Don't include human-readable header in directory listing

--- cfdp-core/src/filestore.rs
+++ cfdp-core/src/filestore.rs
@@ -327,7 +327,7 @@ impl FileStore for NativeFileStore {
     fn list_directory<P: AsRef<Utf8Path>>(&self, path: P) -> FileStoreResult<String> {
         let directory = self.get_native_path(path);
         let mut directory_listing =
-            format!("Listing for directory: {directory}\ntype,path,size,timestamp\n",);
+            format!("type,path,size,timestamp\n",);
         let (mut dirs, mut files): (Vec<_>, Vec<_>) = fs::read_dir(&directory)?
             .filter_map(|entry| entry.ok())
             .partition(|entry| entry.path().is_dir());
